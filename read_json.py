f3c4937703b6a6d71fb7892dcba1862915a20a14
jtliao <jtliao9@yahoo.com>
2016-09-28 00:29:46 -0400

    reads json into a list of json objects

--- read_json.py
+++ read_json.py
@@ -1,5 +1,6 @@
 import json
-
-with open('yelp_academic_dataset_business.json') as data_file:
-    data = json.load(data_file)
-#filtered = [x for x in data if x['state'] == 'PA']
+data = []
+with open('yelp_academic_dataset_business.json') as f:
+    for line in f:
+        data.append(json.loads(line))
+print(data[0])
