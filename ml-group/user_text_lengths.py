59a3969b1e560b4ab2d540451aa710699d0c2bc9
Eric Xiang Ma <exm2@cornell.edu>
2016-11-05 15:40:09 -0400

    examining density plots for users who write the most reviews

--- ml-group/user_text_lengths.py
+++ ml-group/user_text_lengths.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 # from sklearn import linear_model
 from scipy import stats
+import seaborn as sns
+import math
 
 def read_pittsburg_reviews(business_file, reviews_file, out_file):
     # businesses_in_pitt = set()
@@ -22,7 +24,7 @@ def read_pittsburg_reviews(business_file, reviews_file, out_file):
             # if record["business_id"] in businesses_in_pitt:
             if True:
                 ctr += 1
-                print(ctr)
+                # print(ctr)
                 user_id = record["user_id"]
                 review = record["text"]
                 tokens = nltk.word_tokenize(review)
@@ -40,6 +42,63 @@ def read_pittsburg_reviews(business_file, reviews_file, out_file):
             f.write("\n")
 
 
+def sort_by_num_reviews(review_lengths_file):
+    with open(review_lengths_file, "r") as f:
+        all_lines = f.readlines()
+
+        # print(all_lines[0:3])
+
+        all_lines_sorted = sorted(all_lines, key=lambda list_str: len(json.loads(list_str)), reverse=True)
+        # print(len(all_lines[0]))
+        # print(len(all_lines[1]))
+        # print(len(all_lines[2]))
+
+        all_lines_lists = map(lambda list_str: json.loads(list_str), all_lines_sorted)
+
+        all_line_lens = map(lambda list_of_lists: len(list_of_lists), all_lines_lists)
+        all_line_log_lens = map(lambda list_of_lists: math.log(len(list_of_lists)), all_lines_lists)
+        # print(all_line_lens[-1])
+        sns.set_style("whitegrid")
+        sns.kdeplot(np.array(all_line_log_lens))
+        sns.plt.show()
+
+        # Examine where the spikes occur
+
+        # all_lines = map(lambda list_str: len(json.loads(list_str)), all_lines)
+        # print(all_lines[0:50])
+
+        # stars = []
+        # z_lengths = []
+
+        # 1 list for each star rating
+        # stars_len_list = [[], [], [], [], []]
+
+        # Only do for top x users
+        # x = 1
+        # for i in range(0, x):
+        #     record = all_lines_lists[i]
+        #     lengths = []
+        #     for [_,length] in record:
+        #         lengths.append(length)
+        #
+        #     mean = np.mean(lengths)
+        #     std = np.std(lengths)
+        #
+        #     for [star_rating, length] in record:
+        #         stars.append(star_rating)
+        #         z_length = (length - mean) / (std)
+        #         z_lengths.append(z_length)
+        #
+        #         # add to corresponding list
+        #         stars_len_list[star_rating - 1].append(z_length)
+        #
+        # f, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5)
+        # axes = [ax1, ax2, ax3, ax4, ax5]
+        # for i in xrange(5):
+        #     sns.set_style("whitegrid")
+        #     sns.kdeplot(np.array(stars_len_list[i]), ax=axes[i])
+        # plt.show()
+
 def analyze_review_lengths(reviews_file):
     with open(reviews_file, "r") as f:
         stars = []
@@ -50,7 +109,7 @@ def analyze_review_lengths(reviews_file):
 
         for line in f:
             record = json.loads(line)
-            if len(record) >= 10:
+            if len(record) >= 30:
                 lengths = []
                 for [_, length] in record:
                     lengths.append(length)
@@ -63,6 +122,10 @@ def analyze_review_lengths(reviews_file):
 
                     # add to corresponding list
                     stars_len_list[star_rating - 1].append(z_length)
+
+    # sns.set_style("whitegrid")
+    # sns.kdeplot(np.array(stars_len_list[0]))
+    # sns.plt.show()
 
     # plt.scatter(z_lengths, stars)
     # plt.xlabel("Text length normalized by user")
@@ -77,17 +140,30 @@ def analyze_review_lengths(reviews_file):
     print "Std err: %f" % std_err
 
 
+    # for i in xrange(5):
+    #     plt.subplot(3, 2, i + 1)
+    #     plt.hist(stars_len_list[i])
+    #     plt.title(str(i + 1) + " stars")
+    # plt.show()
+
+    f, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5)
+
+    axes = [ax1, ax2, ax3, ax4, ax5]
+
     for i in xrange(5):
-        plt.subplot(3, 2, i + 1)
-        plt.hist(stars_len_list[i])
-        plt.title(str(i + 1) + " stars")
+        sns.set_style("whitegrid")
+        sns.kdeplot(np.array(stars_len_list[i]), ax=axes[i])
     plt.show()
+
+
 
 def main():
     # Just run once to write pitt_reviews file
-    # read_pittsburg_reviews("yelp_academic_dataset_business.json", "yelp_academic_dataset_review.json", "pitt_reviews.json")
+    # read_pittsburg_reviews("yelp_academic_dataset_business.json", "yelp_academic_dataset_review.json", "review_lengths.json")
 
-    analyze_review_lengths("pitt_reviews.json")
+    # analyze_review_lengths("review_lengths.json")
+
+    sort_by_num_reviews("review_lengths.json")
 
 if __name__ == "__main__":
     main()
